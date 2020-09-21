322079d8b0a56fb3c5453410703eaf58ba9b9c21
gahyeon <rkgus8955@likelion.org>
2020-09-21 22:04:42 +0900

    게시물 pk는 아직

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
-def home(request):
-    return render(request,'blog.html')
+def list(request):
+    posts = Post.objects
+    return render(request,'list.html', {'posts':posts})
